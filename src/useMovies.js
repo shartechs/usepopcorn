4360fcc9e6e1729150e838d026f12e40c6cfc931
Shartechs <shartechs@gmail.com>
2023-09-15 00:17:21 +0200

    minor bug fix

--- src/useMovies.js
+++ src/useMovies.js
@@ -19,7 +19,7 @@ export function useMovies(query) {
           setError("");
 
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIES_API_KEY}&s=${query}`,
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
 
